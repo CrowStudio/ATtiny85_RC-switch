e46a7a6ad52b7806d9815da6276b7be77cafd040
Daniel Arvidsson <daniel.arvidsson@crowstudio.se>
2015-05-07 13:54:00 +0200

    Makefile 	fuse_bits_RC-switch

---
+++ RC-switch.c
@@ -0,0 +1,81 @@
+/*
+ * RC-switch.c
+ * 
+ * Copyright 2015 Daniel Arvidsson <daniel.arvidsson@crowstudio.se>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+#include <avr/io.h>
+#include <avr/interrupt.h>
+ 
+#define F_CPU 8000000UL			//defines clock as 8Mhz
+
+int main(void)
+{
+	GIMSK |= (1 << PCIE);		//enable pin change interrupt
+	PCMSK |= (1 << PCINT3); 	//mask PB3 as pin chang interrupt pin
+	sei();			//enable gloabal interrupt
+	
+	DDRB |= (1 << DDB0) | (1 << DDB1) | (1 << DDB2 | 1 << DDB4);		//sets PB0, PB1, PB2 and PB4 as output pins
+
+	TCCR1 |= (1 << CS13); 		 //set counter/timer1 to increment every 16 us
+	
+	while(1)
+	{
+		
+	}
+	
+}
+
+ISR(PCINT0_vect)    	//when pin level changes
+{ 
+	static uint8_t pulse;
+
+	pulse = TCNT1;			//saves counter/timer1 into pulse variable
+	
+	PORTB |= (1 << PORTB4);		//led is on when interrupt is intialized
+
+	if(PINB & (1 << PINB3))			//if PB3 is HIGH
+	{
+		TCNT1 = 0;		//resets counter/timer1
+	}
+	
+	else
+	{
+		if(pulse > 96)			//when stick 1 travels from 1.52 ms towards 2.06 ms
+		{
+			PORTB &= ~(1 << PORTB2);		//relay pole switch, + & - on motor 
+			PORTB |= (1 << PORTB1);			//LED green indicates forward motion
+			PORTB &= ~(1 << PORTB0);		//turn off red LED
+		}
+			
+			else if (pulse < 93) 		//when stick 1 travels from 1.48 ms towards 0.92 ms
+			{
+				PORTB |= (1 << PORTB2);			//relay pole switch, - & + on motor 
+				PORTB &= ~(1 << PORTB1);		//turn off green LED
+				PORTB |= (1 << PORTB0);			//LED red indicates backward motion
+			}	
+			
+		else
+		{
+			
+		}
+		
+	}
+	
+}
