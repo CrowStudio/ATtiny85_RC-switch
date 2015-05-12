3d432f6b5a70b9b5f2c2d15120afb04e4d3a5420
Daniel Arvidsson <daniel.arvidsson@crowstudio.se>
2015-05-12 15:20:27 +0200

    modified:   RC-switch.c

--- RC-switch.c
+++ RC-switch.c
@@ -24,56 +24,60 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
  
-#define F_CPU 8000000UL			//defines clock as 8Mhz
-#define debugPin PORTB4		//defines PB4 as debugPin
+#define F_CPU 8000000UL		 //defines clock as 8Mhz
+
+#define debugPin PORTB4		 //defines PB4 as debugPin
+#define relayPin PORTB2		 //defines PB2 as relayPin
+#define greenLED PORTB1		 //defines PB1 as greenLED
+#define redLED PORTB0		//defines PB0 as redLED
 
 int main(void)
 {
 	GIMSK |= (1 << PCIE);		//enable pin change interrupt
-	PCMSK |= (1 << PCINT3); 	//mask PB3 as pin chang interrupt pin
+	PCMSK |= (1 << PCINT3);		  //mask PB3 as pin chang interrupt pin
 	sei();			//enable global interrupt
 	
 	DDRB |= (1 << DDB0) | (1 << DDB1) | (1 << DDB2 | 1 << DDB4);		//sets PB0, PB1, PB2 and PB4 as output pins
 
-	TCCR1 |= (1 << CS13); 		 //set Timer/Counter1 to increment every 16 us
+	TCCR1 |= (1 << CS13);		//set Timer/Counter1 to increment every 16 us
 	
-	while(1)		//leave blank or put your own code here
+	while(1)		
 	{
-		
+				//leave blank or put your own code here
 	}
 	
 }
 
-ISR(PCINT0_vect)    	//when pin level changes on PB3
+ISR(PCINT0_vect)		//when pin level changes on PB3
 { 
 	static uint8_t pulse;
 
 	pulse = TCNT1;			//saves Timer/Counter1 into pulse variable
 	
-	PORTB |= (1 << debugPin);		//pin is HIGH on when interrupt is intialized
+	PORTB |= (1 << debugPin);		  //pin is HIGH on when interrupt is intialized
 
 	if(PINB & (1 << PINB3))			//if PB3 is HIGH
 	{
-		TCNT1 = 0;		//resets Timer/Counter1
+		TCNT1 = 0;			//resets Timer/Counter1
 	}
 	
 	else
 	{
 		if(pulse > 96)			//when stick 1 travels from 1.52 ms towards 2.06 ms
 		{
-			PORTB &= ~(1 << PORTB2);		//relay pole switch, + & - on motor 
-			PORTB |= (1 << PORTB1);			//LED green indicates forward motion
-			PORTB &= ~(1 << PORTB0);		//turn off red LED
+			PORTB &= ~(1 << relayPin);		  //relay pole switch, + & - on motor 
+			PORTB |= (1 << greenLED);		 //LED green indicates forward motion
+			PORTB &= ~(1 << redLED);		//turn off red LED
 		}
 			
-			else if (pulse < 93) 		//when stick 1 travels from 1.48 ms towards 0.92 ms
+			else if (pulse < 93)		//when stick 1 travels from 1.48 ms towards 0.92 ms
 			{
-				PORTB |= (1 << PORTB2);			//relay pole switch, - & + on motor 
-				PORTB &= ~(1 << PORTB1);		//turn off green LED
-				PORTB |= (1 << PORTB0);			//LED red indicates backward motion
+				PORTB |= (1 << relayPin);		 //relay pole switch, - & + on motor 
+				PORTB &= ~(1 << greenLED);		  //turn off green LED
+				PORTB |= (1 << redLED);			//LED red indicates backward motion
 			}	
 			
-		else 	//if µs is 1480> or <1520 - dead span to prevent glitches on relay when stick is in centre position
+		else 		//if µs is 1480> or <1520 - dead span to prevent glitches on relay when stick is in centre position
 		{
 			
 		}
