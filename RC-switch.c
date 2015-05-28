2968582ba5047308abaf9fd0016fd12f728e79cc
Daniel Arvidsson <daniel.arvidsson@crowstudio.se>
2015-05-28 19:18:35 +0200

    modified:   RC-switch.c

--- RC-switch.c
+++ RC-switch.c
@@ -21,67 +21,111 @@
  * 
  */
  
+ /* 
+ * The ATtiny85 RC-switch reads the PWM-signal on channel one's pin 
+ * (throttle) from a OrangeRx R615 receiver, it works as a polarity 
+ * switch for a DC-motor controlled with a cheap electrical speed 
+ * controller (ESC) that has no reverse. I have programmed my Spectrum 
+ * DX7 radio's throttle stick to have zero speed in the centre and 
+ * forward speed both up and down.
+ * 
+ * The main function of the ATtiny85 will check the position of the 
+ * throttle stick and do the following: 
+ * When you move the stick up, the motor will go into forward motion, 
+ * and when you move the stick back to the centre - 1500 µs - the motor 
+ * stops. Once the stick's position will go below 1480 µs the switch 
+ * will change the polarity of the motor and it will go into reverse 
+ * motion, when it comes back to centre the motor once again stops. 
+ * If you then again move the stick's position above 1520 µs the switch 
+ * will change the polarity back to it's origin and the motor will once 
+ * again go into forward motion.
+ *  
+ */
+ 
 #include <avr/io.h>
 #include <avr/interrupt.h>
  
-#define F_CPU 8000000UL		 //defines clock as 8Mhz
+#define F_CPU 8000000UL			//defines clock as 8Mhz
 
 #define debugPin PORTB4		 //defines PB4 as debugPin
 #define relayPin PORTB2		 //defines PB2 as relayPin
 #define greenLED PORTB1		 //defines PB1 as greenLED
 #define redLED PORTB0		//defines PB0 as redLED
 
+// volatile uint8_t pulse;				// global variable to store pulse length
+volatile uint8_t tot_overflow;			// global variable to count the number of Timer/Counter1 overflows
+volatile uint16_t pulse16;			//global variable to make a 16 bit integer from tot_overflow and TCNT1
+volatile uint8_t pInt;				// global variable to indicate PCINT
+
+
 int main(void)
 {
-	GIMSK |= (1 << PCIE);		//enable pin change interrupt
-	PCMSK |= (1 << PCINT3);		  //mask PB3 as pin chang interrupt pin
-	sei();			//enable global interrupt
-	
 	DDRB |= (1 << DDB0) | (1 << DDB1) | (1 << DDB2 | 1 << DDB4);		//sets PB0, PB1, PB2 and PB4 as output pins
-
-	TCCR1 |= (1 << CS13);		//set Timer/Counter1 to increment every 16 us
 	
-	while(1)		
+	// TCCR1 |= (1 << CS13);		//set Timer/Counter1 to increment every 16 us
+	TCCR1 |= (1 << CS12); 		 //set Counter/Timer1 prescaler to increment every 1µs
+	
+	GIMSK |= (1 << PCIE);		//enable pin change interrupt
+	PCMSK |= (1 << PCINT3); 	//mask PB3 as pin chang interrupt pin
+	TIMSK |= (1 << TOIE1);		//enable Counter/Timer1 overflow interrupt
+	sei();			//enable gloabal interrupt
+	
+	while(1)		//leave or put your own code here
 	{
-				//leave blank or put your own code here
+		while(pInt == 1)			//PCNINT-while-loop
+		{
+			if(PINB & (1 << PINB3))			//if PB3 is HIGH
+			{
+				TCNT1 = 0;		//resets Timer/Counter1
+				tot_overflow = 0;		//resets tot_overflow variable
+			}
+			
+			else 		
+			{ 
+				if (pulse16 >=1520)			//when stick 1 travels from 1520 µs towards 2006 µs
+				//if((tot_overflow == 5 && pulse > 240) || tot_overflow > 6)			
+				{
+					PORTB &= ~(1 << relayPin);		  //relay pole switch, + & - on motor 
+					PORTB |= (1 << greenLED);		 //LED green indicates forward motion
+					PORTB &= ~(1 << redLED);		//turn off red LED
+				}
+				
+				else if (pulse16 <=1480)			//when stick 1 travels from 1480 ms towards 920 µs
+				//	else if((tot_overflow == 5 && pulse < 200) || tot_overflow  < 5)
+					{
+						PORTB |= (1 << relayPin);		 //relay pole switch, - & + on motor 
+						PORTB &= ~(1 << greenLED);		  //turn off green LED
+						PORTB |= (1 << redLED);			//LED red indicates backward motion
+					}	
+				
+				else        //if µs is 1480> or <1520 - dead-span to prevent gliteches on relay when stick is in centre position
+				{ 
+					//PORTB |= (1 << greenLED);			//for debug to indicate dead-span	
+					//PORTB |= (1 << redLED);			//for debug to indicate dead-span
+				}
+						
+			}
+			
+			pInt = 0;		//resets pInt to exit PCNINT-while-loop
+		}
+		
 	}
 	
 }
 
-ISR(PCINT0_vect)		//when pin level changes on PB3
+ISR(TIMER1_OVF_vect)			//when Counter/Timer1 overflows
+{
+    tot_overflow++;			// keeps track of Counter/Timer1's overflow
+}
+
+ISR(PCINT0_vect)    	//when pin level changes on PB3
 { 
-	static uint8_t pulse;
-
-	pulse = TCNT1;			//saves Timer/Counter1 into pulse variable
+// 	pulse = TCNT1;			//saves Timer/Counter1 into pulse variable
 	
-	PORTB |= (1 << debugPin);		  //pin is HIGH on when interrupt is intialized
+	PORTB |= (1 << debugPin);		//pin is HIGH on when interrupt is intialized
 
-	if(PINB & (1 << PINB3))			//if PB3 is HIGH
-	{
-		TCNT1 = 0;			//resets Timer/Counter1
-	} 
+	pInt = 1;			//indicates PCINT
 	
-	else
-	{
-		if(pulse > 96)			//when stick 1 travels from 1.52 ms towards 2.06 ms
-		{
-			PORTB &= ~(1 << relayPin);		  //relay pole switch, + & - on motor 
-			PORTB |= (1 << greenLED);		 //LED green indicates forward motion
-			PORTB &= ~(1 << redLED);		//turn off red LED
-		}
-			
-			else if (pulse < 93)		//when stick 1 travels from 1.48 ms towards 0.92 ms
-			{
-				PORTB |= (1 << relayPin);		 //relay pole switch, - & + on motor 
-				PORTB &= ~(1 << greenLED);		  //turn off green LED
-				PORTB |= (1 << redLED);			//LED red indicates backward motion
-			}	
-			
-		else 		//if µs is 1480> or <1520 - dead span to prevent glitches on relay when stick is in centre position
-		{
-			
-		}
-		
-	}
+	pulse16 = (tot_overflow << 8) | TCNT1;			//adds tot_overflow and TCNT1 to be able to set if-statements in PCINT-while-loop with µs
 	
 }
